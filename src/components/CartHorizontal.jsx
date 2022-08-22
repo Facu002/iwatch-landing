c6e10ed38b23807f23c7df2b3b3c21810cc9d170
Facu002 <achafacundoagustin@gmail.com>
2022-08-22 19:29:23 -0300

    commit-0.2 (desktop H-card finished)

--- src/components/CartHorizontal.jsx
+++ src/components/CartHorizontal.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import img from '../media/activity_watch_hw__f42hdrx2vnqm_large.jpg'
 import vid from '../media/medium_2x.mp4'
+import {Transition, CSSTransition, SwitchTransition, TransitionGroup} from "react-transition-group";
 function CartHorizontal(props) {
 
   const [activeInfo, setActiveInfo] = useState(false)
@@ -23,35 +24,47 @@ function CartHorizontal(props) {
 
     <div className="CartHorizontal-container__box">
 
-      <div className='box'>
-        <h3 className="title">Tu compañero de <br /> <span>entrenamiento</span> <br /> perfecto.</h3>
+      <div className='box1-container'>
+        <h3 className="box1-container__title">Tu compañero de <br /> <span>entrenamiento</span> <br /> perfecto.</h3>
 
-        <div className="artMainJS main-article2">
+        <div className="box1-container__video-container">
           <div>
-          <video src={vid} autoPlay></video>
+            <video className='box1-container__video' src={vid} autoPlay></video>
           </div>
         </div>
 
       </div>
 
-      <div className="box2">
-        <div onClick={()=>setActiveInfo(!activeInfo)}>
-          <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-circle-plus" width="44" height="44" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#000000" fill="none" strokeLinecap="round" strokeLinejoin="round">
+      <div className="box2-container">
+        <div className='box2-container__btn' onClick={()=>setActiveInfo(!activeInfo)}>
+          {
+            !open
+            ? <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-circle-plus" width="44" height="44" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#000000" fill="none" strokeLinecap="round" strokeLinejoin="round" style={{transform:"rotate(0deg)", transition:"transform 0.5s"}}>
             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
             <circle cx="12" cy="12" r="9" />
             <line x1="9" y1="12" x2="15" y2="12" />
             <line x1="12" y1="9" x2="12" y2="15" />
           </svg>
+            :<svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-circle-plus" width="44" height="44" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#359048" fill="#ffff" strokeLinecap="round" strokeLinejoin="round" style={{transform:"rotate(-135deg)", transition:"all 0.5s"}}>
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <circle cx="12" cy="12" r="9" />
+            <line x1="9" y1="12" x2="15" y2="12" />
+            <line x1="12" y1="9" x2="12" y2="15" />
+          </svg>
+          }
+          
+          
         </div >
       </div>
-      {
-        open == true
-        ? <div className='box3'>
-            <h3>Tu compañero de entrenamiento perfecto.</h3>
-            <p>Los círculos de Actividad te muestran todos tus movimientos del día con tres simples mediciones: Moverse, Ejercicio y Pararse. Además, la app Entrenamiento te permite registrar tus ejercicios de Pilates, Bicicleta, Baile o el que más te guste.</p>
-          </div>
-        : null
-      }
+      
+      <CSSTransition in={open} timeout={300} classNames="overlayAnimation" unmountOnExit>
+
+      <div className='overlaybox-container'>
+            <h3 className='overlaybox-container__title'>Tu compañero de entrenamiento perfecto.</h3>
+            <p className='overlaybox-container__text'>Los círculos de Actividad te muestran todos tus movimientos del día con tres simples mediciones: Moverse, Ejercicio y Pararse. Además, la app Entrenamiento te permite registrar tus ejercicios de Pilates, Bicicleta, Baile o el que más te guste.</p>
+      </div>
+
+      </CSSTransition>
 
     </div>
     
